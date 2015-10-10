ca839217b18dc3b1447584364ec076d6c488df05
Michael Andorfer & Bernhard Steger <mandorfer.mmt-b2014@fh-salzburg.ac.at & bsteger.mmt-b2013@fh-salzburg.ac.at>
2015-10-10 17:33:33 +0200

    import all outsourced code so that we get one app

--- 02-modules/src/app.js
+++ 02-modules/src/app.js
@@ -1,6 +1,43 @@
-import Handlebars from 'handlebars'
+/*global jQuery, Handlebars, Router */
+
 import {
-  uuid,
-  pluralize,
-  store
+    ENTER_KEY,
+    ESCAPE_KEY
+} from './keyboard'
+
+import $ from 'jquery'
+
+import Handlebars from 'handlebars'
+
+import {
+    uuid,
+    pluralize,
+    store
 } from './util'
+
+import {
+    init,
+    cacheElements,
+    bindEvents,
+    render,
+    renderFooter,
+    toggleAll,
+    getActiveTodos,
+    getCompletedTodos,
+    getFilteredTodos,
+    destroyCompleted,
+    indexFromEl,
+    create,
+    toggle,
+    edit,
+    editKeyup,
+    update,
+    destroy
+} from './application'
+
+
+init();
+
+
+
+
