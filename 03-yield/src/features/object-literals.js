64cc2c4af60c28832e087f1fe11ad98ad1f74c1c
Michael Andorfer & Alexander Gabriel <mandorfer.mmt-b2014@fh-salzburg.ac.at & agabriel.mmt-b2014@fh-salzburg.ac.at>
2015-10-22 10:05:56 +0200

    add solution object-literals

--- 03-yield/src/features/object-literals.js
+++ 03-yield/src/features/object-literals.js
@@ -8,4 +8,14 @@ export function es5() {
 }
 
 export function es6() {
+    let i = 0
+    function next() {
+        return this.i += 1
+    }
+    const o = {
+        i,
+        next
+    }
+
+    return o;
 }
