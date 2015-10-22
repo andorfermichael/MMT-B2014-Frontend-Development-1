0749b4961a377c657533a2f462e09d11c17e42c6
Michael Andorfer & Alexander Gabriel <mandorfer.mmt-b2014@fh-salzburg.ac.at & agabriel.mmt-b2014@fh-salzburg.ac.at>
2015-10-22 09:52:38 +0200

    add map-set solution

--- 03-yield/test/features/map-set.js
+++ 03-yield/test/features/map-set.js
@@ -27,6 +27,26 @@ export default function() {
     myMap.get('second').should.eql(2)
   })
 
-  it.skip('should show Map/Set behavior in ES6', () => {
+  it('should show Map/Set behavior in ES6', () => {
+    const s = new Set()
+    s
+    .add(1)
+    .add(2)
+    .add(3)
+
+    s.has(1).should.be.true
+    s.has(2).should.be.true
+    s.has(3).should.be.true
+    s.has(4).should.be.false
+
+    const m = new Map()
+    m.set('Hello', 'World!')
+    m.set('second', 2);
+    (function() {
+        m.set(1, 2)
+    }).should.not.throw()
+    m.get('Hello').should.eql('World!')
+    m.get('second').should.eql(2)
+
   })
 }
