e3018063a71c8920880bfec12c9b5f1526c3b088
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2015-11-07 19:48:53 +0100

    [feature] implement table with drivers data

--- 04-dev/app/assets/scripts/js/app.js
+++ 04-dev/app/assets/scripts/js/app.js
@@ -1,5 +1,46 @@
-page('/', '/home')
-page('/home', pages.home)
+import $ from 'jquery'
+import page from 'page'
+import fetch from 'isomorphic-fetch'
+import tplHome from './templates/home.hbs'
+import tplDrivers from './templates/drivers.hbs'
 
-page('/drivers', pages.drivers)
-page('/drivers/:driver', pages.driver)
+const $content = $('#content')
+const $nav = $('.nav')
+
+page('*', function(ctx, next) {
+	$nav
+		.children()
+		.removeClass('active')
+	$nav
+		.find('a[href|="' + ctx.path + '"]')
+		.parent()
+		.addClass('active')
+	next()
+})
+
+function home() {
+	$content.html(tplHome())
+}
+
+function drivers() {
+	fetch('http://ergast.com/api/f1' + '/drivers.json')
+	 .then(response => {
+		if (response.status >= 400) {
+			$content.html('Error')
+		}
+		return response.json()
+	 })
+	.then(data => {
+		$content.html(tplDrivers({drivers: data.MRData.DriverTable.Drivers}))
+	})
+	.catch(err => {
+		$content.html('Error')
+	})
+}
+
+
+
+//page('/', '/home')
+//page('/home', home())
+
+page('/drivers', drivers())
