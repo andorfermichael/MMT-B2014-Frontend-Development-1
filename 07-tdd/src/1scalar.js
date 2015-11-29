ece67f0d422e32292787def67a0279adb0cad2c1
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2015-11-29 16:17:27 +0100

    add test solutions

--- 07-tdd/src/1scalar.js
+++ 07-tdd/src/1scalar.js
@@ -1,12 +1,28 @@
-module.exports = function scalar(x) {
-    if (typeof x === 'number') {
-        return x
-    }
-    if (typeof x === 'string') {
-        return x
-    }
-    if (typeof x === 'boolean') {
-        return x
-    }
-    throw new Error('Does only accept scalar input')
-}
+var should = require('should')
+var scalar = require('../src/1scalar.js')
+
+describe('scalar', function() {
+	it('should take a number as input', function() {
+		should(scalar(10)).be.a.Number()
+	})
+
+	it('should take a string as input', function() {
+		should(scalar('hello')).be.a.String()
+	})
+
+	it('should take a boolean as input', function() {
+		should(scalar(true)).be.a.Boolean()
+	})
+
+	it('should not take a null as input', function() {
+		(function() {
+			scalar(null)
+		}).should.throw()
+	})
+
+	it('should not take an object as input', function() {
+		(function() {
+			scalar({})
+		}).should.throw()
+	})
+})
