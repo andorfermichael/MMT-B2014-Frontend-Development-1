107a9f8d8d3d025fbe4fd591a3d4aa145daceea2
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2015-12-27 18:21:44 +0100

    [refactor] remove case statement from movie and add price variable

--- 08-refactoring/movie.rb
+++ 08-refactoring/movie.rb
@@ -9,18 +9,15 @@ class Movie
 
   attr_reader :title
   attr_reader :price_code
+  attr_writer :price
 
   def price_code=(value)
     @price_code = value
-    @price = case price_code
-      when REGULAR then RegularPrice.new
-      when NEW_RELEASE then NewReleasePrice.new
-      when CHILDRENS then ChildrensPrice.new
-    end
   end
 
   def initialize(title, the_price_code)
     @title, self.price_code = title, the_price_code
+    self.price = the_price_code
   end
 
   def charge(days_rented)
