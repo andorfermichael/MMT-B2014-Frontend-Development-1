444aedd81f8bb664dab8a385a777888d88512769
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2015-12-27 17:02:14 +0100

    [test] add tests for html_statement method

--- 08-refactoring/test.rb
+++ 08-refactoring/test.rb
@@ -55,5 +55,47 @@ You earned 1 frequent renter points
     end 
      
   end
+
+  describe "customer html_statement" do
+
+    it "is printed correctly for a new release movie" do
+      @customer.add_rental(Rental.new(@movie_1,  2))
+      @customer.html_statement.must_equal <<-END.strip
+<h1>Rentals for <em>Vroni</em></h1><p>
+\tIron Man 3: 6<br>
+<p>You owe <em>6</em><p>
+On this rental you earned <em>2</em> frequent renter points<p>
+      END
+    end
+
+    it "is printed correctly for a regular movie" do
+      @customer.add_rental(Rental.new(@movie_2, 3))
+      @customer.html_statement.must_equal <<-END.strip
+<h1>Rentals for <em>Vroni</em></h1><p>
+\tAvatar: 3.5<br>
+<p>You owe <em>3.5</em><p>
+On this rental you earned <em>1</em> frequent renter points<p>
+      END
+    end
+
+    it "is printed correctly for a childrens movies" do
+      @customer.add_rental(Rental.new(@movie_3, 4))
+      @customer.html_statement.must_equal <<-END.strip
+<h1>Rentals for <em>Vroni</em></h1><p>
+\tBrave: 3.0<br>
+<p>You owe <em>3.0</em><p>
+On this rental you earned <em>1</em> frequent renter points<p>
+      END
+    end
+
+    it "is summed up correctly for 3 movies" do
+      @customer.add_rental(Rental.new(@movie_1,  2))
+      @customer.add_rental(Rental.new(@movie_2, 3))
+      @customer.add_rental(Rental.new(@movie_3, 4))
+      @customer.statement.must_match /Amount owed is 12.5/
+      @customer.statement.must_match /You earned 4 frequent renter points/
+    end
+
+  end
 end
 
